a430e36410fa7e58957c1aaf11eb774da5e82f28
Dave Donnally <davenjermon@hotmail.com>
2018-03-15 21:11:10 -0800

    start of zipping implementation

--- application/controllers/Raw_uploads.php
+++ application/controllers/Raw_uploads.php
@@ -183,25 +183,32 @@ class Raw_uploads extends CI_Controller{
 
 	public function download($files)
 	{
-		foreach($files as $file => $file_name)
-		{
-			$file_path=$this->file_dir.'/raw/'.$file_name;
-			if (file_exists($file_path)) 
-			{
-			    header('Content-Description: File Transfer');
-			    header('Content-Type: application/octet-stream');
-			    header('Content-Disposition: attachment; filename="'.basename($file_path).'"');
-			    header('Expires: 0');
-			    header('Cache-Control: must-revalidate');
-			    header('Pragma: public');
-			    header('Content-Length: ' . filesize($file_path));
-			
-			    readfile($file_path);
-			   // exit;
-			}
-			//exit;
-			
-		}
+	    if(count($files) == 1)
+	    {
+	        foreach($files as $file => $file_name)
+	        {
+	            $file_path=$this->file_dir.'/raw/'.$file_name;
+	            if (file_exists($file_path))
+	            {
+	                header('Content-Description: File Transfer');
+	                header('Content-Type: application/octet-stream');
+	                header('Content-Disposition: attachment; filename="'.basename($file_path).'"');
+	                header('Expires: 0');
+	                header('Cache-Control: must-revalidate');
+	                header('Pragma: public');
+	                header('Content-Length: ' . filesize($file_path));
+	                
+	                readfile($file_path);
+	                // exit;
+	            }
+	            //exit;
+	        }
+	    }
+	    else
+	    {
+	        
+	    }
+
 		// exit;
 		$this->index();
 	}
